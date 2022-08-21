47042779015ec9e94539866992620d9fbc1d64ff
Martin Blasko <mblasko42@gmail.com>
2022-08-21 19:48:59 +0200

    lil cleanup and fix

--- Sequence.lua
+++ Sequence.lua
@@ -17,21 +17,23 @@
             If the length is larger than the current length, 
             the sequence should be with the new length filled values copied from the current sequence using modulo arithmetic.
 
-]]--
+]] --
 
 
 --[[
     The Sequence class.
-]]--
+]] --
 Sequence = {}
 Sequence.__index = Sequence
 
 
-Sequence.new = function(self, sequence)
+Sequence.new = function(self)
     local obj = {}
     setmetatable(obj, Sequence)
-    obj.sequence = sequence
-    obj.sequence_index = 0
+    obj.length = 0
+    obj.index = 1
+    obj.sequence = {}
+    obj.index = 0
     return obj
 end
 
@@ -39,31 +41,41 @@ end
 --[[
     The output function.
     It should return the current output of the sequence.
-]]--
+]] --
 Sequence.output = function(self)
-    return self.sequence[self.sequence_index]
+    return self.sequence[self.index]
 end
 
 --[[
     The set_sequence function.
     It should set the sequence to the given array.
-]]--
-   
+]] --
+
 Sequence.set_sequence = function(self, sequence)
     self.sequence = sequence
+    self.length = #sequence
+
 end
 
+Sequence.set_step = function(self, step, val)
+    self.sequence[step] = val
+end
+Sequence.set_index = function(self, index)
+    self.index = index
+end
 
 --[[
     The step function.
     It should step the sequence to the next element, in case of a loop it should step back to the first element.
-]]--
+]] --
 
 Sequence.step = function(self)
-    self.sequence_index = self.sequence_index + 1
-    if self.sequence_index >= #self.sequence then
-        self.sequence_index = 0
+
+    self.index = self.index + 1
+    if self.index > self.length then
+        self.index = 1
     end
+
 end
 
 Sequence.set_length = function(self, length)
@@ -78,4 +90,8 @@ Sequence.set_length = function(self, length)
         end
     end
 end
-
+Sequence.clear = function(self)
+    for i = 0, #self.sequence do
+        self.sequence[i] = 0
+    end
+end
